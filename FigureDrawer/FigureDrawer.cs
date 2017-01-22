0f73b54feb925dc1b8feae4b66f238878092dfc9
JesusSalasMataro <jesus459@hotmail.es>
2017-01-22 17:20:35 +0100

    Version 1.0

---
+++ FigureDrawer/FigureDrawer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FigureDrawer
+{
+    public class FigureDrawer
+    {
+        public void DrawSquare(int number)
+        {
+            for (int row = 1; row <= number; row++)
+            {
+                DrawLine(row, number);
+            }
+        }
+
+        public void DrawTriangle(int numero)
+        {
+            int numColumnas = CalcularNumeroColumnas(numero);
+
+            for (int fila = 1; fila <= numero; fila++)
+            {
+                for (int columna = 1; columna <= numColumnas; columna++)
+                {
+                    if (CasillaSeraEspacioEnBlanco(fila, columna, numero))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(fila.ToString());
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+        
+
+        public void DrawTriangleTurbo(int numero) 
+        {
+            for (int fila = 1; fila <= numero; fila++)
+            {
+                for (int columna = 1; columna <= numero + fila - 1; columna++)
+                {
+                    if (CasillaSeraEspacioEnBlanco(fila, columna, numero))
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(fila.ToString());
+                    }
+                }
+
+                Console.WriteLine();
+            }        
+        }
+
+        private bool CasillaSeraEspacioEnBlanco(int fila, int columna, int numero)
+        {
+            return columna <= numero - fila || columna >= numero + fila;
+        }
+
+        private int CalcularNumeroColumnas(int numero)
+        {
+            return numero * 2 - 1;
+        }
+
+        private void DrawLine(int row, int number)
+        {
+            for (int column = 1; column <= number; column++)
+            {
+                if (PositionIsPerimeter(row, column, number) || PositionIsDiagonal(row, column))
+                {
+                    Console.Write(row.ToString() + " ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private bool PositionIsDiagonal(int row, int column)
+        {
+            return row == column;
+        }
+
+        private bool PositionIsPerimeter(int row, int column, int number)
+        {
+            return row == 1 || row == number || column == 1 || column == number;
+        }
+    }
+}
---
+++ FigureDrawer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureDrawer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FigureDrawer squareDrawer = new FigureDrawer();
+
+            int number = AskForNumber();
+
+            squareDrawer.DrawSquare(number);
+            Console.WriteLine();
+            squareDrawer.DrawTriangle(number);
+            Console.WriteLine();
+            squareDrawer.DrawTriangleTurbo(number);
+
+            Console.WriteLine();
+            Console.WriteLine("Pulsa cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+
+        private static int AskForNumber()
+        {
+            Console.Write("Introduce un número: ");
+
+            int number = 0;
+            int.TryParse(Console.ReadLine(), out number);
+
+            Console.WriteLine();
+            
+            return number;
+        }
+    }
+}
